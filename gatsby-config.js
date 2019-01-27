13a57260a48d3c6fcd9dcb037e8c9422b75c1d3c
matt <matt.pocock@thevirtualforge.com>
2019-01-27 20:55:41 +0000

    Pushed the manifest and offline plugin work

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Typescript Starter`,
+    title: `Day Tracker`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // Add typescript stack into webpack
     `gatsby-plugin-typescript`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Day Tracker',
+        short_name: 'Tracker',
+        start_url: '/',
+        background_color: '#9dc7c8',
+        theme_color: '#9dc7c8',
+        display: 'standalone',
+        icon: 'src/images/website-icon.png', // This path is relative to the root of the site.
+      },
+    },
   ],
 };
